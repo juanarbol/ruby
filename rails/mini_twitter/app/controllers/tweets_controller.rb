63ff9a1e311f2e08112f1b7d523fe26fb2bafd20
Juan José <juan.jose.arboleda.a@gmail.com>
2016-02-24 20:26:00 -0500

    mini-twitter con post y show

---
+++ rails/mini_twitter/app/controllers/tweets_controller.rb
@@ -0,0 +1,33 @@
+class TweetsController < ApplicationController
+	def index
+		@tweets = Tweet.all
+	end
+
+	def new
+		@tweet = Tweet.new
+	end
+
+	def create
+		@tweet = Tweet.new(tweets_params)
+		@tweet.save
+		redirect_to tweets_path
+	end
+
+	def edit
+		@tweet = Tweet.find(params[:id])
+	end
+
+	def update
+		@tweet = Tweet.find(params[:id])
+		@tweet = Tweet.update(tweets_params)
+		redirect_to tweets_path
+	end
+
+	def destroy
+	end
+
+	def tweets_params
+		params.require(:tweet).permit(:message)
+	end
+
+end
