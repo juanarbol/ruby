231f6e05780068b415889c1bbe9fed3b357b88c6
Juan José <juan.jose.arboleda.a@gmail.com>
2016-02-25 19:33:04 -0500

    Avances en bases de datos

--- rails/mini_twitter/db/schema.rb
+++ rails/mini_twitter/db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225004945) do
+ActiveRecord::Schema.define(version: 20160225214715) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.string   "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "nombre"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
